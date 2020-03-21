9475e3c251a56b51833644cd59623a91a9bfa0bd
Thomas Marchand <thomas.marchand@tuta.io>
2020-03-21 17:42:33 +0100

    :tada: initial commit

---
+++ storage/atto/__init__.py
@@ -0,0 +1,108 @@
+import os
+import json
+import pickle
+from pathlib import Path
+
+
+# allows to serialize sets, see https://stackoverflow.com/a/8230373
+
+class PythonObjectEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (list, dict, str, int, float, bool, type(None))):
+            return json.JSONEncoder.default(self, obj)
+        return {'_python_object': pickle.dumps(obj)}
+
+def as_python_object(dct):
+    if '_python_object' in dct:
+        return pickle.loads(str(dct['_python_object']))
+    return dct
+
+def get_data_path(name):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), "data", name)
+
+class Database:
+
+    def __init__(self, name):
+        self.name = name
+        self.path = get_data_path(name)
+        self.load_data()
+
+    def load_data(self):
+
+        if self.name.endswith(".py") or self.name.startswith("."): # should never be triggered
+            return
+
+        if os.path.exists(self.path):
+            with open(self.path) as json_file:
+                data = json.load(json_file, object_hook=as_python_object)
+                self.available_identifiers = data["free"]
+                self.groups = data["groups"]
+                self.items = data["items"]
+                return
+
+        self.available_identifiers = []
+        self.groups = {}
+        self.items = []
+
+    def save_data(self):
+
+        data = { "free" : self.available_identifiers, "groups" : self.groups, "items" : self.items }
+
+        print(data)
+
+        Path( os.path.dirname(self.path) ).mkdir(parents=True, exist_ok=True)
+        with open(self.path, 'w') as outfile:
+            json.dump(data, outfile, cls=PythonObjectEncoder)
+
+    def _find_smallest_space(self, spaces_name):
+        minimal_space = self.groups[spaces_name[0]]
+        if len(spaces_name) > 1:
+            for space_name in spaces_name:
+                space = self.groups[space_name]
+                if len(space) < len(minimal_space):
+                    minimal_space = space
+        return minimal_space
+
+    def inter(self, *spaces_name):
+        smallest_space = self._find_smallest_space(spaces_name)
+        condition_spaces = [space_name for space_name in spaces_name if space_name != smallest_space]
+
+        results = []
+
+        for item_id in smallest_space:
+            data = self.items[ item_id ]
+            if all(conditional_space in data[1] for conditional_space in condition_spaces):
+                results.append(data)
+
+        return results
+
+    def sum(self, *spaces_name):
+        result_set = set()
+        for space_name in spaces_name:
+            result_set = result_set | self.groups[space_name]
+        print( result_set )
+
+
+    def add_data(self, data):
+        if len(self.available_identifiers) > 0:
+            id = self.available_identifiers.remove[0]
+            self.items[id] = data
+        else:
+            id = len(self.items)
+            self.items.append(data)
+        
+        for space_name in data[1]:
+            if space_name in self.groups:
+                space = self.groups[space_name]
+                space.add( id )
+            else:
+                self.groups[space_name] = { id }
+        self.save_data()
+
+    def remove_data(self, data):
+        id = self.items.index(data)
+        for space in data[1]:
+            self.groups[space].remove(id)
+        self.items[id] = None
+        self.available_identifiers.append(id)
+        self.save_data()
