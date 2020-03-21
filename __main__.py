ea69cdd8547669e393f97ea79b0a06564078cd4b
Thomas Marchand <thomas.marchand@tuta.io>
2020-03-21 17:41:28 +0100

    :construction: trying to use atto

--- __main__.py
+++ __main__.py
@@ -2,10 +2,12 @@ import os
 from aiohttp import web
 from storage import atto
 
-def get_path(name):
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), name)
-
 # CONSTANTS
 SERVER_URL = "https://s1.nexmind.space/"
 
 db = atto.Database("yolo")
+example_data = ( 18773893988, set(["abc", "def", "ghi"]) )
+db.add_data(example_data)
+
+print(db.inter("abc"))
+print(db.sum("def"))
